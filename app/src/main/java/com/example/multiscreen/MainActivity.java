23784e4daaf6abdf4dfc8fdfc396875624043ef5
Pramod Silwal <silwalpramod99@gmail.com>
2021-11-27 12:44:38 +0545

    second commit

--- app/src/main/java/com/example/multiscreen/MainActivity.java
+++ app/src/main/java/com/example/multiscreen/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edittext;
+    public static final String HELLO_text="com.example.multiscreen.extra_hello";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
     public void show(View view){
         //Toast.makeText(MainActivity.this, "Helllo", Toast.LENGTH_SHORT).show();
         Log.d("btton","No error");
+        edittext=findViewById(R.id.editText);
+        String name=edittext.getText().toString();
         Intent intent=new Intent(this,MainActivity2.class);
+        intent.putExtra(HELLO_text,name);
         startActivity(intent);
 
 
--- app/src/main/java/com/example/multiscreen/MainActivity2.java
+++ app/src/main/java/com/example/multiscreen/MainActivity2.java
@@ -2,13 +2,20 @@ package com.example.multiscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
+        textView=findViewById(R.id.textView);
+        Intent intent=getIntent();
+        String b=intent.getStringExtra(MainActivity.HELLO_text);
+        textView.setText("Happy Birthday to you "+b);
     }
 }
